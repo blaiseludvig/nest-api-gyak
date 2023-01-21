90da009e70ff2ad81d8468e982002fc5ff27e543
blaiseludvig <blaiseludvig@gmail.com>
2023-01-21 04:29:22 +0100

    add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
@@ -7,12 +8,15 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   /*
      Here it's assumed that public and views are in the root directory,
-     alongside with src. You can put them wherever you want, 
+     alongside with src. You can put them wherever you want,
      just use the correct path if you use another folder.
   */
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+  // Use ValidationPipe
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
