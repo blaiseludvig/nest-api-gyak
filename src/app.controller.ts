4745b59f61f959f6857b6fbddf9d5e786005709e
blaiseludvig <blaiseludvig@gmail.com>
2023-01-21 04:31:34 +0100

    add GET /api/cats route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,24 @@
-import {
-  Controller,
-  Get,
-  Render,
-} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import GetCatsDto from './GetCats.dto';
 import db from './db';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Get('/api/cats')
+  async allUsers(@Query() query: GetCatsDto) {
+    let sort = query.sort;
+    sort = sort ? sort : 'id';
+    console.log(sort);
+
+    const [cats] = await db.execute(`
+    SELECT suly, szem_szin
+    FROM macskak
+    ORDER BY ${sort} ASC
+    `);
+
+    return { cats: cats };
   }
 }
